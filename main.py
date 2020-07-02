efb3ebd2f83e15af45fb524aa9857ce9858fcde5
HimanshuMittal01 <44058642+HimanshuMittal01@users.noreply.github.com>
2020-07-02 07:48:06 +0530

    Can choose file

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import os
 from tkinter import *
+from tkinter import filedialog
 from src.layer import Layer
+import torch
 
 root = Tk()
 canvas = Canvas(root,bg="white",height=600,width=600)
 canvas.pack()
 
+def openfile():
+    currdir = os.getcwd()
+    tempdir = filedialog.askopenfilename(parent=root, initialdir=currdir, title='Please select a file')
+    if len(tempdir) > 0:
+        print("You chose %s" % tempdir)
+
+button = Button(root, padx=50, text="Click me!!", command=openfile)
+button.pack()
+
 root.update()
 layer = Layer(50, canvas)
 layer.show()
 
-if __name__=='__main__':
-    root.mainloop()
+root.mainloop()
+
+# if __name__=='__main__':
+    
+#     root.mainloop()
---
+++ src/modelmanager.py
@@ -0,0 +1,3 @@
+class ModelManager:
+    def __init__(self, filepath):
+        self.filepath = filepath
